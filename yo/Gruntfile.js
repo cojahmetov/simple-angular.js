29f32101d43a6caec1cbd562bfe6dc1f7fe7e24d
dman777 <darin.hensley@gmail.com>
2014-04-27 19:04:07 -0500

    sss

--- yo/Gruntfile.js
+++ yo/Gruntfile.js
@@ -1,18 +1,51 @@
 module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt);
     grunt.initConfig({
-	pkg: grunt.file.readJSON('package.json'),
-	watch {
-	    servedFiles: {
-		files : '<%= pkg.name %>/static/**'
-	    },
-	    options: {
-	      livereload: {
-	          port: 9000,
-		  hostname: 0.0.0.0,
+        pkg: grunt.file.readJSON('package.json'),
 
+        express: {
+           prod: {
+               options: {
+                   port: 9000,
+                   script: 'app.js',
+	           fallback: function () {
 
-        
+               }
+           }
+        },
+
+	open: {
+	    delayed: {
+		path: 'http://127.0.0.1:9000',
+	        app: 'Google Chrome',
+	        options: {
+		    openOn: 'serverListening'
+	        }
+            }
+	},
+
+        watch: {
+            options: {
+                livereload: true
+            },
+
+	    express: {
+		files: ['<%= pkg.name %>/static/**',
+			'app.js'],
+		tasks: ['express:prod'],
+		options: {
+		    spawn: false
+		}
+	    }
+        }
     });
-
+    grunt.registerTask('default', 'start server', function() {
+        grunt.task.run('express');
+        console.log("ssss");
+        grunt.task.run('watch');
+	if (!err) {
+	    grunt.event.emit('serverListening');
+	    }
+    });
+}
 
--- yo/app.js
+++ yo/app.js
@@ -2,9 +2,10 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-      res.send('Hello World');
+      res.send('Hello ');
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(9000, function() {
         console.log('Listening on port %d', server.address().port);
 });
+
