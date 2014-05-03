49dcdcef2d63ab61e8e1dae6d5ea5c47c744aa35
dman777 <darin.hensley@gmail.com>
2014-05-03 03:12:49 +0000

    fixed static directory location

--- yo/app.js
+++ yo/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 
-app.use(express.static(__dirname + '/static'));
+app.use(express.static(__dirname + '/app/static'));
 
 var server = app.listen(9000, function() {
         console.log('Listening on port %d', server.address().port);
