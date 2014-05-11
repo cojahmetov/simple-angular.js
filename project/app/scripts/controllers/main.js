45e7ec34507fd5dfb3e2b65cedcf3e7c7c9520b6
dman777 <darin.hensley@gmail.com>
2014-05-11 00:56:19 -0500

    took out old code

--- project/app/scripts/controllers/main.js
+++ project/app/scripts/controllers/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-darinsApp.controller('Scroller', function($scope, $location, $document, $anchorScroll) {
+darinsApp.controller('Scroller', function($scope, $location, $document) {
     $scope.scrollTo = function(id) {
         var el = angular.element(document.getElementById(id));
         $document.scrollTo(el, 2000);
